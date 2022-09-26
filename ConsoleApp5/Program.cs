3e8c58b8290bc56bdcc4eae5857ff266778384be
Bhattacharya.Rounak <Bhattacharya.Rounak@INLPF37B1HY>
2022-09-26 12:05:16 +0530

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Q5
+{
+    abstract class BankAcc
+    {
+        public abstract void Link_Aadhar_AccNo();
+
+    }
+    class Link : BankAcc
+    {
+        int adh_no;
+        public Link()
+        {
+            Console.Write("Enter a Aadhar No : ");
+            adh_no = Convert.ToInt32(Console.ReadLine());
+
+        }
+        public override void Link_Aadhar_AccNo()
+        {
+            Console.WriteLine("Aadhar number link to Bank Account Successfully!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Link link = new Link();
+
+            link.Link_Aadhar_AccNo();
+        }
+    }
+}
