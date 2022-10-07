f92dfda3822ac5a859bb480ea7b6d71da316f003
Ron7978 <72132759+Ron7978@users.noreply.github.com>
2022-10-07 15:54:27 +0530

    Add files via upload

---
+++ Asyn_Pgm/Asyn_Pgm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asyn_Pgm
+{
+    internal class Program
+    {
+
+        static async void LongProcess()
+        {
+            Console.WriteLine("LongProcess started");
+            //Thread.Sleep(4000);
+            Console.WriteLine("LongProcess Completed");
+        }
+
+        public static async void ShortPreocess()
+        {
+            Console.WriteLine("ShortProcess Started");
+            //await Task.Delay(1000);
+            Console.WriteLine("ShortProcess Completed");
+        }
+        static async Task Main(string[] args)
+        {
+            LongProcess();
+            ShortPreocess();
+
+            Console.ReadLine();
+        }
+        //Console.ReadLine();
+    }
+}
