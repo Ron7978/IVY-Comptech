74f1ed1c63cdf257ea2e71d9b1a99318c074d88a
Ron7978 <72132759+Ron7978@users.noreply.github.com>
2022-10-07 15:40:30 +0530

    Add files via upload

---
+++ Struct_Pointer/Struct_Pointer/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Struct_Pointer
+{
+
+    struct cube
+    {
+        double side;
+        public cube(double a)
+        {
+            side = a;
+        }
+        public double ShowVolume()
+        {
+            double v = side * side * side;
+            return v;
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            unsafe
+            {
+                cube* pointTocube;
+                cube mycube = new cube(7.6);
+                pointTocube = &mycube;
+
+                //MessageBox.Show("Volume of cube = " + (*pointTocube).ShowVolume());
+                MessageBox.Show("Volume of cube = " + pointTocube->ShowVolume());
+            }
+        }
+    }
+}
